0f859d2cbbc37afee9e03e6bddb7c2fd6c79a42d
Debajyoti Shit <ds2250@it.jgec.ac.in>
2023-04-09 00:35:38 +0530

    first commit

---
+++ src/assets/images/images.js
@@ -0,0 +1,31 @@
+import wallpaper1 from "./wallpaper1.webp";
+import wallpaper2 from "./wallpaper2.webp";
+import wallpaper3 from "./wallpaper3.jpg";
+import wallpaper4 from "./wallpaper4.jpg";
+import wallpaper5 from "./wallpaper5.jpg";
+import coverflow from "./coverflow.png";
+import games from "./games.jpeg";
+import allsongs from "./allsongs.gif";
+import artists from "./artists.png";
+import albums from "./albums.png";
+import song1Img from "./Shape_Of_You.png";
+import song2Img from "./calm Down.webp";
+import song3Img from "./bones.jpg";
+
+const images = {
+	wallpaper1,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+	coverflow,
+	games,
+	allsongs,
+	artists,
+	albums,
+	song1Img,
+	song2Img,
+	song3Img,
+};
+
+export default images;
---
+++ src/assets/music/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./Shape-Of-You.mp3";
+import music2 from "./Calm-Down.mp3";
+import music3 from "./Imagine-Dragons-Bones.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
